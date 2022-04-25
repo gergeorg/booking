d99d3bf0e264c70b1502c6f9015697de501b6789
gergeorg <georgey.gerasimov@gmail.com>
2022-04-25 14:14:47 +0300

    create forms

---
+++ js/modules/start.js
@@ -0,0 +1,67 @@
+import createElement from './createElement.js';
+
+const createTitle = (title) => {
+  const h1 = createElement('h1', {
+    className: 'title',
+    textContent: title,
+  });
+
+  return h1;
+}
+
+const createMain = () => {
+  const main = createElement('main', {
+    className: 'person-data',
+  });
+
+  return main;
+}
+
+const createFirstForm = () => {
+  const form = createElement('form', {
+    className: 'field',
+  })
+
+  const label = createElement('label', {
+    className: 'field__label',
+    textContent: 'Укажите количество человек (max: 6)'
+  })
+
+  const input = createElement('input', {
+    className: 'field__input',
+    id: 'count',
+    name: 'count',
+    type: 'number',
+    placeholder: 'Укажите количество человек',
+    min: '1',
+    max: '6',
+    required: true,
+  })
+
+  const button = createElement('button', {
+    className: 'btn-confirm',
+    type: 'submit',
+    textContent: 'Подтвердить',
+  })
+
+  form.append(label, input, button);
+
+  return form;
+}
+
+const start = (app, title) => {
+  const h1 = createTitle(title);
+  const main = createMain();
+  const firstForm = createFirstForm();
+
+  app.append(h1, main);
+  main.append(firstForm);
+
+  return {
+    main,
+    firstForm
+  };
+}
+
+
+export default start;
